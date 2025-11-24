bd08ebe03dad74d4383f007d281fbefe6db62dcf
Ambar <144835836+Ambar-Goyal@users.noreply.github.com>
2025-11-24 16:44:34 +0530

    Update CORS middleware configuration

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const dataRoutes = require('./routes/dataRoutes');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 app.use(morgan('dev'));
 
